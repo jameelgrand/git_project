5b27d626fbc1b410daca88ace7a162f7c45d14ef
jameelgrand <jameelgrand@gmail.com>
2014-10-14 15:44:05 +0530

    file override technique added for file uploads

--- importings/models.py
+++ importings/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+import os
+from django.core.files.storage import FileSystemStorage
 # Create your models here.
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name):
+        if self.exists(name):
+            os.remove(os.path.join('', name))
+        return name
 class dbcred(models.Model):
 	cred_name = models.CharField(max_length=80,primary_key=True)
 	database = models.CharField(max_length=80)
@@ -13,7 +19,7 @@ class dbcred(models.Model):
 	def __unicode__(self):
 		return self.cred_name
 class File(models.Model):
-    file =  models.FileField(upload_to='importings/static/files/')
+    file =  models.FileField(upload_to='importings/static/files/',storage=OverwriteStorage())
     def __unicode__(self):
         return self.file.name.split('/')[3]
 class activedb(models.Model):
